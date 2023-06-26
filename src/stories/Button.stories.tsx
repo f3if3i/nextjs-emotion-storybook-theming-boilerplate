4090587dabd1f132e26f735d702babad8687c376
feifei0914kwc <yufeifei.li@kddi-web.com>
2023-06-26 15:57:12 +0900

    Button stories

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { Button } from "../../components/Button"
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof Button> = {
+    title: "Example/Button",
+    component: Button,
+    tags: ["autodocs"],
+    
+}
+
+export default meta
+type Story = StoryObj<typeof Button>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+    args: {
+        label: "Button",
+    },
+}
+
+export const Secondary: Story = {
+    args: {
+        label: "Button",
+    },
+}
